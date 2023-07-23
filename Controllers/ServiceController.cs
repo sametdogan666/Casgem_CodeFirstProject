7f7cc309ffa8ca087cb8f03c41ca07f6a7bf4e15
Samet DOGAN <sametdogan23@protonmail.ch>
2023-07-23 14:48:04 +0300

    service page added

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using Casgem_CodeFirstProject.DAL.Context;
+using System.Web.Mvc;
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly TravelContext _travelContext = new TravelContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialInnerService()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialOuterService()
+        {
+            return PartialView();
+        }
+    }
+}
